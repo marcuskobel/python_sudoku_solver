125f08a1918cddaa77021966a8a50db192ff5203
marcuskobel <marcus.kobel@gmail.com>
2020-12-06 21:00:36 -0300

    Final code clean up. Should work perfectly.

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -9,26 +9,20 @@ class Sudoku_Solver:
 
 
     def is_sudoku_valid(self):
-        # check if current sudoku grid configuration is valid or not
-        # uses full grid scans for all numbers - very slow
-        # logdebug("SUDOKU SOLVER", "Starting horizontal check...", False)
+        # check if current sudoku grid configuration is valid or not, uses full grid scans for all numbers
         for n in range(1, 10):
             for i in range(0, 9):
                 nfoundhori = False
                 nfoundvert = False
                 for j in range(0, 9):
-                    # logdebug("SUDOKU SOLVER", "Horizontally checking number = "+str(n)+" at position "+str(i)+","+str(j)+" = "+str(sudoku_grid[i][j]), False)
                     if self.sudoku_grid[i][j] == n:
                         if nfoundhori:
-                            # logdebug("SUDOKU SOLVER", "Issue found!! Sudoku is invalid.", False)
                             return False
                         else:
                             nfoundhori = True
 
-                    # logdebug("SUDOKU SOLVER", "Vertically checking number = "+str(n)+" at position "+str(j)+","+str(i)+" = "+str(sudoku_grid[j][i]), False)
                     if self.sudoku_grid[j][i] == n:
                         if nfoundvert:
-                            # logdebug("SUDOKU SOLVER", "Issue found!! Sudoku is invalid.", False)
                             return False
                         else:
                             nfoundvert = True
@@ -42,7 +36,7 @@ class Sudoku_Solver:
             for j in range(0, 9):
                 if self.sudoku_grid[i][j] > 0:
                     count += 1
-        # logdebug("SUDOKU SOLVER", "Amount numbers found in sudoku = " + str(count), False)
+
         if count == 81:
             logdebug("SUDOKU SOLVER", "SODUKU SOLUTION FOUND! Number of tries = "+str(self.tries), False)
             self.print_sudoku_grid()
@@ -62,7 +56,6 @@ class Sudoku_Solver:
             print('')
 
 
-
     def check_if_number_works_at_position(self, number, line, column):
         # check if number is duplicated in both line and column
         for i in range(0, 9):
@@ -77,10 +70,9 @@ class Sudoku_Solver:
 
     def solve_sudoku(self, iteration_level, lin, col):
         # main function that tries to solve sudoku
-        if col == 8 and lin == 8:
-            nextlin = 9
-            nextcol = 9
-        elif col == 8:
+
+        # calculate next lin/col that will
+        if col == 8:
             nextlin = lin + 1
             nextcol = 0
         else:
@@ -98,7 +90,6 @@ class Sudoku_Solver:
                         # sudoku is resolved (hopefully!), print it and get out
                         return True
 
-                    # logdebug("SUDOKU SOLVER", "LVL"+str(iteration_level)+" - assigning "+str(t)+" at position "+str(i)+","+str(j) + " and restarting recursion", False)
                     if self.tries % 20000 == 0:
                         logdebug("SUDOKU SOLVER", "# of tries = "+str(self.tries), False)
                         if self.tries % 100000 == 0:
@@ -110,11 +101,6 @@ class Sudoku_Solver:
                     else:
                         self.sudoku_grid[lin][col] = 0
 
-                # else:
-                    # logdebug("SUDOKU SOLVER", "LVL"+str(iteration_level)+" - possiility "+str(t)+" didnt work at position " + str(i) + "," + str(j), False)
-                    # if num == 9:
-            # if FOR loop reached end and haven't found a solution, goes back in the recursion
-            # self.sudoku_grid[lin][col] = 0
             return False
 
         else:
